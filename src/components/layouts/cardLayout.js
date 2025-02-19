061f60590aca9e888b5fd6faaf9ab710ee5bb3ee
James Arn <132308884+arnsocks@users.noreply.github.com>
2025-02-18 19:05:46 -0500

    Change the way the task info cards are created.

--- src/components/layouts/cardLayout.js
+++ src/components/layouts/cardLayout.js
@@ -9,31 +9,72 @@ export default function TaskCards(taskList) {
     const taskCard = document.createElement('div');
     taskCard.classList.add('task-card');
     taskCard.dataset.taskID = task.id;
-    
+
+    const taskInfoList = document.createElement('ul');
+    taskInfoList.classList.add('task-info-list');
+
+    // Create Task Card Heading
+    const titleItem = document.createElement('li');
     const myTitle = document.createElement('h2');
     myTitle.textContent = task.title;
-    const myDueDate = document.createElement('p');
-    myDueDate.textContent = `Due: ${task.dueDate}`;
-    const myDescription = document.createElement('p');
-    myDescription.textContent = `Description: ${task.description}`;
-    const myPriority = document.createElement('p');
-    myPriority.textContent = `Priority: ${task.priority}`;
-    myPriority.classList.add("priority", `${task.priority}`);
+    titleItem.appendChild(myTitle);
 
+    //Create Due Date
+    const dueItem = document.createElement('li');
+    const myDueDate = document.createElement('span');
+    const dueDateHeading = document.createElement('span');
+    dueDateHeading.classList.add('task-info-heading');
+    dueDateHeading.textContent = 'Due: '
+    myDueDate.textContent += `${task.dueDate}`;
+    dueItem.appendChild(dueDateHeading);
+    dueItem.appendChild(myDueDate);
 
-    const myProject = document.createElement('p');
+    // Create description
+    const descriptionItem = document.createElement('li');
+    const myDescription = document.createElement('span');
+    const descriptionHeading= document.createElement('span');
+    descriptionHeading.classList.add('task-info-heading');
+    descriptionHeading.textContent = 'Description: ';
+    myDescription.textContent = `${task.description}`;
+    descriptionItem.appendChild(descriptionHeading);
+    descriptionItem.appendChild(myDescription);
+
+    // Create priority
+    const priorityItem = document.createElement('li');
+    const myPriority = document.createElement('span');
+    const priorityHeading= document.createElement('span');
+    priorityHeading.classList.add('task-info-heading');
+    priorityHeading.textContent = 'Description: ';
+    myPriority.textContent = `${task.priority}`;
+    priorityItem.appendChild(priorityHeading);
+    priorityItem.appendChild(myPriority);
+
+    // Create Project 
+    const projectItem = document.createElement('li');
+    const myProject = document.createElement('span');
+    const projectHeading = document.createElement('span');
+    projectHeading.classList.add('task-info-heading');
+    projectHeading.textContent = 'Project: ';
     const projectObj = APP.getProjectByID(task.project);
     let projectName = 'No project assigned';
     if (projectObj) projectName = `${projectObj.title}`;
-    myProject.textContent = `Project: ${projectName}`;
+    myProject.textContent = `${projectName}`;
+    projectItem.appendChild(projectHeading);
+    projectItem.appendChild(myProject);
 
-    taskCard.appendChild(myTitle);
-    taskCard.appendChild(myDescription);
-    taskCard.appendChild(myDueDate);
-    taskCard.appendChild(myPriority);
-    taskCard.appendChild(myProject);
+    taskInfoList.appendChild(titleItem);
+    taskInfoList.appendChild(descriptionItem);
+    taskInfoList.appendChild(dueItem);
+    taskInfoList.appendChild(priorityItem);
+    taskInfoList.appendChild(projectItem);
+    taskCard.appendChild(taskInfoList);
     cardContainer.appendChild(taskCard);
   };
 
   return cardContainer;
 }
+
+
+const titleHeading = document.createElement('span');
+titleHeading.classList.add('task-info-heading');
+titleHeading.textContent = ''
